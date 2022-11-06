b13a0e2f4531a8e343cfabf22fd77502d91d10fc
Luciano Paranhos <luciano@paranhos.com.br>
2022-11-05 21:59:07 -0300

    adicionado exercicio cap 02

--- src/bin/chapter_02/main.rs
+++ src/bin/chapter_02/main.rs
@@ -1,3 +1,32 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello from chapter 02!");
+    println!("Advinhe o número");
+    let secret_value = rand::thread_rng().gen_range(1..101);
+    println!("O número secreto: {}", secret_value);
+
+    loop {
+        println!("Digite um número: ");
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("Erro ao ler");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Seu palpite: {}", guess);
+
+        match guess.cmp(&secret_value) {
+            Ordering::Less => println!("Muito pequeno!"),
+            Ordering::Greater => println!("Muito grande!"),
+            Ordering::Equal => {
+                println!("Você ganhou");
+                break;
+            }
+        }
+    }
 }
