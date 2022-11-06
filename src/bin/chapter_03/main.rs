28f47dee98f24b982d11cd467b0ba10eb1270ca2
Luciano Paranhos <luciano@paranhos.com.br>
2022-11-06 18:51:18 -0300

    add chapter 03

---
+++ src/bin/chapter_03/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+  let x = 50;
+  println!("O valor de x é: {}", x);
+
+  let y = x;
+  println!("O valor de y é: {}", y);
+
+  let x = 60;
+  println!("O valor de x é: {}", x);
+
+  println!("O valor de y é: {}", y);
+
+  const MAX_POINT: u32 = 100_000;
+  println!("O valor de MAX_POINT é: {}", MAX_POINT);
+
+}
